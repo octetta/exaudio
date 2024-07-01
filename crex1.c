9ac7fdfc32ebaaba68ab6ff0e03e788f0b437e35
octetta <joseph.stewart@gmail.com>
2024-06-30 22:29:56 -0700

    clean-up. prep for tuple write back to elixir

--- crex1.c
+++ crex1.c
@@ -1,5 +1,6 @@
 #include <endian.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -44,12 +45,14 @@ int descending(ccrContParam, int fd) {
 
 enum {
   exa_nil,
-  exa_list,
-  exa_binary
+  exa_int, // stored in val
+  exa_list, // stored in len/list
+  exa_binary // stored in blob/list
 } exa_type;
 
 struct exa_tuple {
   uint8_t type;
+  int32_t val;
   char key[KEY_STORE];
   uint8_t count;
   int32_t number;
@@ -60,6 +63,35 @@ struct exa_tuple {
 };
 
 #define CR "\n\r"
+
+void exa_dump(struct exa_tuple *tuple) {
+  if (!tuple) return;
+  char *s = "";
+  LOG("{");
+  if (tuple->key) LOG("\"%s\"", tuple->key);
+  switch (tuple->type) {
+    case exa_nil:
+      break;
+    case exa_int:
+      LOG(",%d", tuple->val);
+      break;
+    case exa_list:
+      LOG(",[");
+      for (int i=0; i<tuple->len; i++) {
+        LOG("%s%d", s, tuple->list[i]);
+        s = ",";
+      }
+      LOG("]");
+      break;
+    case exa_binary:
+      LOG(",\"%s\"", tuple->blob);
+      break;
+    default:
+      LOG("?"CR);
+      break;
+  }
+  LOG("}"CR);
+}
 
 /*
 
@@ -96,206 +128,233 @@ ETF STU LEN BIN L3  L2  L1  L0  r   e   t   -   o   u   t   s   LIE L3  L2  L1  
 ETF STU LEN BIN L3  L2  L1  L0  i   n   f   o   BIN L3  L2  L1  L0  o   k   a   y
 */
 
-int free_list_fail(struct exa_tuple *kv, int r) {
-  if (kv) {
-    if (kv->list) {
-      free(kv->list);
-      kv->blob = NULL;
-    }
-    kv->len = 0;
+enum {
+  read_okay = 100,
+  read_empty,
+  read_error,
+  
+  read_fd_negative,
+  read_null_struct,
+
+  read_bad_magic,
+  read_not_tuple,
+  read_bad_tuple_len,
+  read_bad_ext,
+  read_bad_val,
+
+  read_bad_len,
+  read_unknown,
+
+} read_errors;
+
+static int _read_real_error = 0;
+
+int read_real_error(void) {
+  return _read_real_error;
+}
+
+int readb4(int fd, uint32_t *four) {
+  uint32_t n = 0;
+  int r = read(fd, &n, sizeof(uint32_t));
+  _read_real_error = r;
+  if (r < 0) return -read_error;
+  if (r == 0) return -read_empty;
+  if (r != sizeof(uint32_t)) return -read_bad_len;
+  n = be32toh(n);
+  if (four) *four = n;
+  return read_okay;
+}
+
+int readb2(int fd, uint16_t *two) {
+  uint16_t n;
+  int r = read(fd, &n, sizeof(uint16_t));
+  _read_real_error = r;
+  if (r < 0) return -read_error;
+  if (r == 0) return -read_empty;
+  if (r != sizeof(uint16_t)) return -read_bad_len;
+  if (two) *two = be16toh(n);
+  return read_okay;
+}
+
+int readb1(int fd, uint8_t *one) {
+  uint8_t n = 0;
+  int r = read(fd, &n, sizeof(uint8_t));
+  _read_real_error = r;
+  if (r < 0) return -read_error;
+  if (r == 0) return -read_empty;
+  if (r != sizeof(uint8_t)) return -read_bad_len;
+  if (one) *one = n;
+  return read_okay;
+}
+
+int readpast(int fd, size_t skip) {
+  uint8_t ignore;
+  for (int i=0; i<skip; i++) {
+    int r = read(fd, &ignore, sizeof(ignore));
+    _read_real_error = r;
+    if (r < 0) return -read_error;
+    if (r == 0) return -read_empty;
+    if (r != skip) return -read_bad_len;
+  }
+  return read_okay;
+}
+
+int free_list_fail(struct exa_tuple *tuple, int r) {
+  if (tuple && tuple->list) {
+    free(tuple->list);
+    tuple->list = NULL;
+    tuple->len = 0;
   }
   return r;
 }
 
-int receiver(int fd, struct exa_tuple *kv) {
-  LOG("receiver"CR);
+enum {
+  write_okay = 100,
+} write_errors;
 
-  if (fd < 0) return -1;
-  if (!kv) return -1;
-  
-  kv->type = exa_nil;
-  kv->count = 0;
-  if (kv->list) free(kv->list);
-  kv->list = NULL;
-  if (kv->blob) free(kv->blob);
-  kv->blob = NULL;
-  kv->len = 0;
+int exa_send(int fd, struct exa_tuple *tuple) {
+  return write_okay;
+}
 
-  uint8_t b;
-  uint32_t u;
-  int32_t i;
-  uint16_t s;
+int exa_parse(int fd, struct exa_tuple *tuple) {
+  if (fd < 0) return -read_fd_negative;
+  if (!tuple) return -read_null_struct;
+ 
+  tuple->key[0] = '\0';
+  tuple->type = exa_nil;
+  tuple->count = 0;
+  if (tuple->list) free(tuple->list);
+  tuple->list = NULL;
+  if (tuple->blob) free(tuple->blob);
+  tuple->blob = NULL;
+  tuple->len = 0;
+
+  uint8_t one;
+  uint32_t four;
+  uint16_t two;
 
   int r;
   
   // match magic
-  r = read(fd, &b, sizeof(b));
-  if (r < 0) return r;
-  if (b != ETF_MAGIC) return -1;
-  LOG("ETF_MAGIC"CR);
-
+  if (readb1(fd, &one) < 0 || one != ETF_MAGIC) return -read_bad_magic;
   // match small tuple
-  r = read(fd, &b, sizeof(b));
-  if (r < 0) return r;
-  if (b != SMALL_TUPLE_EXT) return -1;
-  LOG("SMALL_TUPLE_EXT"CR);
-
-  // match len == 2
-  r = read(fd, &b, sizeof(b));
-  if (r < 0) return r;
-  //if (b != 2) return -1;
-  kv->count = b;
-  LOG("tuple count == %d"CR, kv->count);
-  if (kv->count == 0) {
-    return 0;
-  }
-
+  if (readb1(fd, &one) < 0 || one != SMALL_TUPLE_EXT) return -read_not_tuple;
+  // get tuple len
+  if (readb1(fd, &one) < 0 || one > 2) return -read_bad_tuple_len;
+  tuple->count = one;
+  if (tuple->count == 0) return read_okay;
   // match bin
-  r = read(fd, &b, sizeof(b));
-  if (r < 0) return r;
-  if (b != BINARY_EXT) return -1;
-  LOG("BINARY_EXT"CR);
-
+  if (readb1(fd, &one) < 0 || one != BINARY_EXT) return -read_bad_ext;
   // match 4 byte len
-  r = read(fd, &u, sizeof(u));
-  if (r != 4) return -1;
-
-  uint32_t len = be32toh(u);
+  if (readb4(fd, &four) < 0) return -read_bad_len;
+  uint32_t len = four;
   uint32_t skip = 0;
-
-  LOG("len = %d"CR, len);
-
   if (len > KEY_MAX) {
     skip = len - KEY_MAX;
     len = KEY_MAX;
-    LOG("adjusted to len = %d"CR, len);
   }
-  r = read(fd, kv->key, len);
-  if (r != len) return -1;
-  if (skip) {
-    LOG("skipping %d bytes of key"CR, skip);
-    uint8_t ignore;
-    for (int i=0; i<skip; i++) {
-      r = read(fd, &ignore, sizeof(ignore));
-      if (r < 0) return r;
-    }
-  }
+  if (read(fd, tuple->key, len) < len) return -read_bad_len;
+  if (skip && readpast(fd, skip) < 0) return -read_bad_len;
 
-  LOG("key = %s"CR, kv->key);
-  if (kv->count == 1) {
-    return 0;
-  }
+  // make sure key is null-terminated
+  tuple->key[len] = '\0';
 
-  // next we can get
-  // NIL = no list follows
-  // STE = simple < 65536 elements of bytes
-  // LIE = list of SIE and INE then NIL
-  // BIN = binary blob
+  if (tuple->count == 1) return read_okay;
+
+  // next we look for...
+  //
+  // NIL_EXT = no list follows
+  // SMALL_INTEGER_EXT = one uint8_t
+  // INTEGER_EXT = one int32_t
+  // BINARY_EXT = binary blob of uint8_t
+  // STRING_EXT = simple list up to 65535 uint8_t
+  // LIST_EXT = list of either uint8_t or int32_t
   
-  // decide next thing
+  if (readb1(fd, &one) < 0) return -read_bad_ext;
 
-  r = read(fd, &b, sizeof(b));
-  if (r < 0) return r;
-
-  switch (b) {
+  switch (one) {
     case NIL_EXT:
-      LOG("NIL_EXT"CR);
-      kv->type = exa_nil;
-      return 0;
-      break;
+      tuple->type = exa_nil;
+      return read_okay;
+    case SMALL_INTEGER_EXT:
+      if (readb1(fd, &one) < 0) return -read_bad_len;
+      tuple->type = exa_int;
+      tuple->val = one;
+      return read_okay;
+    case INTEGER_EXT:
+      if (readb4(fd, &four) < 0) return -read_bad_len;
+      tuple->type = exa_int;
+      tuple->val = four;
+      return read_okay;
     case BINARY_EXT:
-      LOG("BINARY_EXT"CR);
-      kv->type = exa_binary;
-      r = read(fd, &u, sizeof(u));
-      if (r != 4) return -1;
-      len = be32toh(u);
-      LOG("blob len = %d"CR, len);
-      kv->len = len;
+      if (readb4(fd, &four) < 0) return -read_bad_len;
+      len = four;
       if (len) {
-        kv->blob = (uint8_t *)malloc((len+1) * sizeof(uint8_t));
-        if (kv->blob) {
-          r = read(fd, kv->blob, len);
-          if (r < 0) {
-            free(kv->blob);
-            kv->blob = NULL;
-            return r;
+        tuple->blob = (uint8_t *)malloc((len+1) * sizeof(uint8_t));
+        if (tuple->blob) {
+          if (read(fd, tuple->blob, len) != len) {
+            free(tuple->blob);
+            tuple->blob = NULL;
+            return -read_bad_len;
           }
-          kv->blob[len] = '\0';
-          LOG("blob <<%s>>"CR, kv->blob);
-          return 0;
+          tuple->type = exa_binary;
+          tuple->len = len;
+          tuple->blob[len] = '\0';
+          return read_okay;
         }
-      } return 0;
+      }
       break;
     case STRING_EXT:
-      LOG("STRING_EXT"CR);
-      kv->type = exa_list;
-      r = read(fd, &s, sizeof(s));
-      if (r != 2) return -1;
-      len = be16toh(s);
-      LOG("list len = %d"CR, len);
-      kv->len = len;
+      if (readb2(fd, &two) < 0) return -read_bad_len;
+      len = two;
       if (len) {
-        kv->list = (int32_t *)malloc(len * sizeof(int32_t));
-        if (kv->list) {
+        tuple->list = (int32_t *)malloc(len * sizeof(int32_t));
+        if (tuple->list) {
           for (int i=0; i<len; i++) {
-            r = read(fd, &b, sizeof(b));
-            if (r < 0) return free_list_fail(kv, r);
-            LOG("[%d] = %d"CR, i, b);
-            kv->list[i] = b;
+            if (readb1(fd, &one) < 0) return free_list_fail(tuple, -read_bad_val);
+            tuple->list[i] = one;
           }
-          return 0;
+          tuple->type = exa_list;
+          tuple->len = len;
+          return read_okay;
         }
       }
       break;
     case LIST_EXT:
-      LOG("LIST_EXT"CR);
-      kv->type = exa_list;
-      r = read(fd, &u, sizeof(u));
-      if (r != 4) return -1;
-      len = be32toh(u);
-      LOG("list len = %d"CR, len);
-      kv->len = len;
+      if (readb4(fd, &four) < 0) return -read_bad_len;
+      len = four;
       if (len) {
-        kv->list = (int32_t *)malloc(len * sizeof(int32_t));
-        if (kv->list) {
+        tuple->list = (int32_t *)malloc(len * sizeof(int32_t));
+        if (tuple->list) {
           for (int i=0; i<len; i++) {
             int32_t n = 0;
-            r = read(fd, &b, sizeof(b));
-            if (r != 1) return free_list_fail(kv, -1);
-            if (b == SMALL_INTEGER_EXT) {
-              LOG("SMALL_INTEGER_EXT"CR);
-              r = read(fd, &b, sizeof(b));
-              if (r != 1) return free_list_fail(kv, -1);
-              n = b;
-            } else if (b == INTEGER_EXT) {
-              LOG("INTEGER_EXT"CR);
-              r = read(fd, &u, sizeof(u));
-              if (r != 4) return free_list_fail(kv, -1);
-              n = u;
+            if (readb1(fd, &one) < 0) return free_list_fail(tuple, -read_bad_ext);
+            if (one == SMALL_INTEGER_EXT) {
+              if (readb1(fd, &one) < 0) return free_list_fail(tuple, -read_bad_val);
+              n = one;
+            } else if (one == INTEGER_EXT) {
+              if (readb4(fd, &four) < 0) return free_list_fail(tuple, -read_bad_val);
+              n = four;
             } else {
-              LOG("unexpected %d"CR, b);
-              return free_list_fail(kv, -1);
+              LOG("unexpected %d"CR, one);
+              return free_list_fail(tuple, -read_bad_ext);
             }
             LOG("[%d] = %d"CR, i, n);
-            kv->list[i] = n;
+            tuple->list[i] = n;
           }
-          r = read(fd, &b, sizeof(b));
-          if (r != 1) return free_list_fail(kv, -1);
-          if (b != NIL_EXT) {
-            LOG("expected NIL_EXT"CR);
-            return free_list_fail(kv, -1);
-          }
-          LOG("NIL_EXT"CR);
-          return 0;
-        } return -1;
+          if (readb1(fd, &one) < 0) return free_list_fail(tuple, -read_bad_ext);
+          if (one != NIL_EXT) return free_list_fail(tuple, -read_bad_ext);
+          tuple->type = exa_list;
+          tuple->len = len;
+          return read_okay;
+        }
       }
       break;
     default:
-      LOG("unexpected %d"CR, b);
-      return -1;
+      LOG("unexpected %d"CR, one);
+      return -read_bad_ext;
   }
-
-  return -1;
+  return -read_unknown;
 }
 
 void cleaner(void) {
@@ -306,7 +365,6 @@ void cleaner(void) {
 
 int main(int argc, char *argv[]) {
   atexit(cleaner);
-  int i;
 
 #if 0
   do {
@@ -319,37 +377,37 @@ int main(int argc, char *argv[]) {
     i = descending(&z, -1);
     LOG("got %d"CR, i);
   } while (z);
-
-  uint8_t buffer[1024];
-  int array[256];
-  int array_length;
-
-  // Read the encoded data from stdin (Elixir port)
-  // int bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer));
-  // if (bytes_read < 0) {
-  //   LOG("read error %s\n\r", strerror(errno));
-  //   return -1;
-  // }
 #endif
 
-  struct exa_tuple kv;
-  kv.blob = NULL;
-  kv.list = NULL;
-  kv.type = exa_nil;
-  kv.len = 0;
-  kv.count = 0;
+  struct exa_tuple tuple;
 
-  do {
-    i = receiver(STDIN_FILENO, &kv);
-  } while (i >= 0);
+  tuple.blob = NULL;
+  tuple.list = NULL;
+  tuple.type = exa_nil;
+  tuple.len = 0;
+  tuple.count = 0;
 
-  LOG("exit with i=%d"CR, i);
+  int fd = STDIN_FILENO;
 
-  if (kv.blob) free(kv.blob);
-  if (kv.list) free(kv.list);
+  pid_t parent = getppid();
 
-  fflush(stderr);
-  fflush(stdout);
+  while (1) {
+    if (parent != getppid()) {
+      LOG("parent changed!"CR);
+      break;
+    }
+    if (exa_parse(fd, &tuple) == read_okay) {
+      exa_dump(&tuple);
+    } else if (read_real_error() <= 0) {
+      LOG("read error <%s>"CR, strerror(errno));
+      break;
+    }
+  }
+
+  LOG("exit receive loop"CR);
+
+  if (tuple.blob) free(tuple.blob);
+  if (tuple.list) free(tuple.list);
 
   return 0;
 }
